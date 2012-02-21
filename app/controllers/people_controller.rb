d5963d41db67d81d6504bda69f01e60bd37962cb
Chetan Bademi <chetanabademi@gmail.com>
2012-02-21 23:18:55 +0530

    Basic rails app with autocomplete using soulmate!

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,9 @@
+class PeopleController < ApplicationController
+  def index
+  end
+
+  def autocomplete_person_name
+     render :json => Person.search(params['term'])
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soulmate::Application.config.secret_token = 'b6b00d0cedd09c6926b7c875d5bbab4a1fe26f3cca1d9caba139a4aac7aab8350879684b14776a598fabc30fc3a2dd21d24e52174b1bfe5e9f95afab25c919ce'
